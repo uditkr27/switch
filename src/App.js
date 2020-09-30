015adb04b06f6a74c004d2eab4348f61a454504a
V-Sharma10 <2017ugcs071@nitjsr.ac.in>
2020-09-30 19:52:22 +0530

    MainContext Created using createContext

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState, useMemo} from 'react';
 import './App.css';
+import { MainContext } from './Context/MainContext';
 
 function App() {
+
+  const [user,setUser] = useState(null);
+  const providerUser = useMemo(() => ({user,setUser}),[user,setUser])
+
   return (
+    <MainContext.Provider value={providerUser}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+
+     
     </div>
+    </MainContext.Provider>
   );
 }
 
